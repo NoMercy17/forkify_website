12e835998702e40516d221e38615b3b5493160e9
Antonio <101660252+NoMercy17@users.noreply.github.com>
2026-03-06 19:18:47 +0200

    Delete project/src/js/config.js

--- project/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://api.spoonacular.com/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '5bb2e2927f154e43b7a075fcffbcbbe1';
-export const MODAL_CLOSE_SEC = 2.5;
